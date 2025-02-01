dbf2e19c6116fa636203bf2c497241d4c40ad91f
brunotxrs <elielsonbrunno@gmail.com>
2025-02-01 17:56:06 -0400

    feat: create the interaction of card wolverine

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,13 @@
-let navBat = document.getElementById('nav-bat');
 let navNone = document.getElementById('nav-none');
-
+let navBat = document.getElementById('nav-bat');
 navBat.addEventListener('click' , () => {
     navNone.style.display = 'none';
 
     let batmanFirst = document.getElementById('batman-first');
     batmanFirst.style.display = 'block';
 
-    let home = document.getElementById('home');
-    home.addEventListener('click' , () => {
+    let batHome = document.getElementById('bat-home');
+    batHome.addEventListener('click' , () => {
         location.href = 'index.html';
     });
 
@@ -19,8 +18,38 @@ navBat.addEventListener('click' , () => {
         batmanFirst.style.display = 'none';
     });
 
-    let homeMovie = document.getElementById('home-movie');
-    homeMovie.addEventListener('click' , () => {
+
+    let homeBat = window.document.getElementById('home-bat');
+    homeBat.addEventListener('click', () => {
+    location.href = 'index.html';
+
+    });
+  
+});
+
+
+let navWol = document.getElementById('nav-wol');
+navWol.addEventListener('click' , () => {
+    navNone.style.display = 'none';
+
+    let wolverineFirst = document.getElementById('wolverine-first');
+    wolverineFirst.style.display = 'block';
+
+    let wolHome = document.getElementById('wol-home-1');
+    wolHome.addEventListener('click' , () => {
+        location.href = 'index.html';
+    });
+
+    let wolMovie = document.getElementById('wol-movie');
+    wolMovie.addEventListener('click' , () => {
+        let wolverineMovie = document.getElementById('wolverine-movie');
+        wolverineMovie.style.display = 'block';
+        wolverineFirst.style.display = 'none';
+    }); 
+
+    let wolHome2 = document.getElementById('wol-home-2');
+    wolHome2.addEventListener('click' , () => {
         location.href = 'index.html';
     });
 });
+
